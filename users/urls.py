74f544209e99df795035706767e97f0899e2ab97
izhar <ihansari2016@gmail.com>
2024-06-19 19:35:33 +0530

    commented few unnecessary lines

--- users/urls.py
+++ users/urls.py
@@ -2,11 +2,11 @@ from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from users.views import UserList, UserCreate, UserUpdate, UserDelete, UserFind, UserViewSet
 
-router = DefaultRouter()
-router.register(r'users', UserViewSet, basename='user')
+# router = DefaultRouter()
+# router.register(r'users', UserViewSet, basename='user')
 
 urlpatterns = [
-    path('', include(router.urls)),
+    # path('', include(router.urls)),
     path('users/', UserList.as_view(), name='list_users'),
     path('users/add/', UserCreate.as_view(), name='add_user'),
     path('users/update/<int:pk>/', UserUpdate.as_view(), name='update_user'),
